2ef88fd438054e343e40bf9d15a95266865a489b
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-03-21 15:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const elems = document.querySelectorAll('td.price')
+const prices = document.querySelectorAll(".price");
+
+let total = 0;
+
+prices.forEach(price => {
+  total += Number(price.textContent);
+});
+
+const table = document.querySelector("table");
+const newRow = document.createElement("tr");
+
+const newCell = document.createElement("td");
+newCell.colSpan = 2; // span across both columns
+newCell.textContent = `Total Price: ${total}`;
+
+newRow.appendChild(newCell);
+
+table.appendChild(newRow);
 
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
