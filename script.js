ba571559ddd5dd254dc9d9942f4247a928921520
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-03-21 15:29:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const elems = document.querySelectorAll('td.price')
+
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let s=0;
+	elems.forEach((userItem) => {
+		s+=userItem;
+	}
+	return s;
 };
 
 getSumBtn.addEventListener("click", getSum);
