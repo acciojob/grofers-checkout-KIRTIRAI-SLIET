bf072ec598b2695ed4ddb7cbb7068920145efe80
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-03-31 11:24:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let total = 0;
-
 const getSum = () => {
-//Add your code here
+	let total = 0;
+
+	const prices = document.querySelectorAll(".prices");
+
 	prices.forEach(price => {
-	total += Number(price.textContent);
+		total += Number(price.textContent);
 	});
+
+	const table = document.querySelector("table");
+
+	const newRow = document.createElement("tr");
+	const newCell = document.createElement("td");
+
+	newCell.colSpan = 2;
+	newCell.textContent = `Total Price: ${total}`;
+
+	newRow.appendChild(newCell);
+	table.appendChild(newRow);
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-	const prices = document.querySelectorAll(".prices");
-	
-	const table = document.querySelector("table");
-	const newRow = document.createElement("tr");
-	
-	const newCell = document.createElement("td");
-	newCell.colSpan = 2; // span across both columns
-	newCell.textContent = `Total Price: ${total}`;
-	
-	newRow.appendChild(newCell);
-	
-	table.appendChild(newRow);
-	
+
 	const getSumBtn = document.createElement("button");
 	getSumBtn.append("Get Total Price");
 	document.body.appendChild(getSumBtn);
 
-    getSumBtn.addEventListener("click", getSum);
+	getSumBtn.addEventListener("click", getSum);
 });
